e97cce3bf02938541cec471f3883a8fdbc8489bb
Frank Le-Nhu <frank@FrankBook-Pro.local>
2021-07-18 16:33:49 -0700

    For Loop exercises

--- d5_forLoops/avrgHeight.py
+++ d5_forLoops/avrgHeight.py
@@ -1,1 +1,14 @@
 
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+students = 0
+total_height = 0
+for height in student_heights:
+  total_height += height
+  students +=1
+
+avg = round(total_height / students)
+
+print(avg)
---
+++ d5_forLoops/fizzBuzz.py
@@ -0,0 +1,10 @@
+
+for n in range(0,101):
+  if n % 15 == 0:
+        print("FizzBuzz")
+  elif n % 3 == 0:
+    print("fizz")
+  elif n % 5 == 0:
+    print("buzz")
+  else:
+    print(n)
